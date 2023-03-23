664963d16dc4876bf6ad43f3354bdec59dda3a0e
bingtang <123@gmail.com>
2023-03-23 21:31:24 +0800

    第二次修改，上一个错误attuibuteerror应该是把setting.py文件中的spider_opened函数删了导致的

--- tencent/tencent/middlewares.py
+++ tencent/tencent/middlewares.py
@@ -10,6 +10,7 @@ from scrapy import signals
 from itemadapter import is_item, ItemAdapter
 from scrapy.http import HtmlResponse
 
+
 class TencentDownloaderMiddleware:
     # Not all methods need to be defined. If a method is not defined,
     # scrapy acts as if the downloader middleware does not modify the
@@ -34,7 +35,7 @@ class TencentDownloaderMiddleware:
         #   installed downloader middleware will be called
         return None
 
-    #通过该方法对响应对象进行拦截，篡改后使其返回动态加载之后的响应对象
+    # 通过该方法对响应对象进行拦截，篡改后使其返回动态加载之后的响应对象
     def process_response(self, request, response, spider):
         # Called with the response returned from the downloader.
 
@@ -43,20 +44,20 @@ class TencentDownloaderMiddleware:
         # - return a Request object
         # - or raise IgnoreRequest
 
-        #由于我们只需要它处理板块内的响应对象，所以需要判断一下是不是需要处理的
-        #response对应request，request对应url
-        bro=spider.bro#获取了在爬虫类中定义的浏览器对象
+        # 由于我们只需要它处理板块内的响应对象，所以需要判断一下是不是需要处理的
+        # response对应request，request对应url
+        bro = spider.bro  # 获取了在爬虫类中定义的浏览器对象
         if request.url in spider.models_urls:
-            bro.get(request.url)#不同板块所对应的url进行请求
+            bro.get(request.url)  # 不同板块所对应的url进行请求
             sleep(2)
-            page_text=bro.page_source
-            #针对定位到的这些response进行篡改
-            #实例化一个新的响应对象，要包含动态加载出来的新闻内容
-            new_response=HtmlResponse(url=request.url,body=page_text,encoding='utf-8',request=request)
-            #我们需要用selenium来得到包含动态加载内容的
-            #所以需要实例化一个浏览器对象
-            #不可能每执行一次函数就实例化一次
-            #所以去爬虫主文件里去写
+            page_text = bro.page_source
+            # 针对定位到的这些response进行篡改
+            # 实例化一个新的响应对象，要包含动态加载出来的新闻内容
+            new_response = HtmlResponse(url=request.url, body=page_text, encoding='utf-8', request=request)
+            # 我们需要用selenium来得到包含动态加载内容的
+            # 所以需要实例化一个浏览器对象
+            # 不可能每执行一次函数就实例化一次
+            # 所以去爬虫主文件里去写
             return new_response
         else:
             return response
@@ -69,4 +70,8 @@ class TencentDownloaderMiddleware:
         # - return None: continue processing this exception
         # - return a Response object: stops process_exception() chain
         # - return a Request object: stops process_exception() chain
+        pass
+
+    def spider_opened(self, spider):
+        # do some initialization when the spider is opened here
         pass
--- tencent/tencent/spiders/tencent.py
+++ tencent/tencent/spiders/tencent.py
@@ -2,41 +2,42 @@ import scrapy
 from selenium import webdriver
 from tencent.items import TencentItem
 
+
 class TencentSpider(scrapy.Spider):
     name = "tencent"
     allowed_domains = ["news.qq.com"]
-    start_urls = ["http://news.qq.com/"]
-    models_urls=[]#存储相应板块对应详情页的url
+    start_urls = ["https://news.qq.com/"]
+    models_urls = []  # 存储相应板块对应详情页的url
 
-    #实例化一个浏览器对象
+    # 实例化一个浏览器对象
     def __init__(self):
-        self.bro=webdriver.ChromiumEdge(executable_path='../../../msedgedriver.exe')
+        self.bro = webdriver.ChromiumEdge(executable_path='msedgedriver.exe')
 
     def parse(self, response):
-        li_list=response.xpath('/html/body/div/div[1]/div/div/div[1]/div/ul/li')
-        alist=[2,3,4]#所需要得财经、科技、娱乐板块所在位置
+        li_list = response.xpath('/html/body/div/div[1]/div/div/div[1]/div/ul/li')
+        alist = [2, 3, 4]  # 所需要得财经、科技、娱乐板块所在位置
         for index in alist:
-            model_url=li_list[index].xpath('./a/@href').extract_first()
+            model_url = li_list[index].xpath('./a/@href').extract_first()
             self.models_urls.append(model_url)
 
-        #依次对每一个板块对应的页面进行请求
-        for url in self.models_urls:#对每一个板块url进行请求发送
-            yield scrapy.Request(url,callback=self.parse_model)
+        # 依次对每一个板块对应的页面进行请求
+        for url in self.models_urls:  # 对每一个板块url进行请求发送
+            yield scrapy.Request(url, callback=self.parse_model)
 
-    #每个板块的新闻都是动态加载出来的，所以必须要处理response
-    def parse_model(self,response):#解析每一个板块页面中对应的新闻内容的url
-        li_list=response.xpath('/html/body/div/div[4]/div[2]/div/div/ul/li')
+    # 每个板块的新闻都是动态加载出来的，所以必须要处理response
+    def parse_model(self, response):  # 解析每一个板块页面中对应的新闻内容的url
+        li_list = response.xpath('/html/body/div/div[4]/div[2]/div/div/ul/li')
         for li in li_list:
-            news_detail=li.xpath('./div/div[1]/h3/a/@href').extract_first()
-            #对新闻的具体内容发出请求
-            yield scrapy.Request(url=news_detail,callback=self.parse_detail())
+            news_detail = li.xpath('./div/div[1]/h3/a/@href').extract_first()
+            # 对新闻的具体内容发出请求
+            yield scrapy.Request(url=news_detail, callback=self.parse_detail())
 
-    def parse_detail(self,response):#解析新闻内容
-        content=response.xpath('/html/body/div[3]/div[1]/div[1]/div[2]//text()').extract()
-        content=''.join(content)
-        item=TencentItem()
-        item['content']=content
+    def parse_detail(self, response):  # 解析新闻内容
+        content = response.xpath('/html/body/div[3]/div[1]/div[1]/div[2]//text()').extract()
+        content = ''.join(content)
+        item = TencentItem()
+        item['content'] = content
         yield item
 
-    def closed(self,spider):
+    def closed(self, spider):
         self.bro.quit()
